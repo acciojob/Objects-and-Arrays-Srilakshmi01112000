7819e3eeb1960d590cc542cd142db06e85025393
Sri Lakshmi Iyengar K R <110050350+Srilakshmi01112000@users.noreply.github.com>
2023-08-01 12:27:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+function copyArray(arr) {
+  return [...arr];
+}
 
+const team = copyArray(players);
+const team1 = copyArray(players);
+
+const cap1 = { ...person };
+
+console.log(team); // Output: ["John", "Bob", "Alice", "Poppy"]
+console.log(team1); // Output: ["John", "Bob", "Alice", "Poppy"]
+console.log(cap1); // Output: { name: "John Doe", age: 80 }
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
